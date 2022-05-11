8bdb08ec2fd3fe3b4642e5b7b9afa5800ce65ca2
Lukasz Wojcik <arbor@10g.pl>
2022-05-11 22:12:33 +0200

    init

---
+++ alpaki-psycho/src/components/Cart.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="drop-shadow-md hover:drop-shadow-xl w-full">
+    <div class="bg-white rounded-lg p-4">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue } from "vue-class-component";
+
+export default class Cart extends Vue {}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
---
+++ alpaki-psycho/src/views/Host.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="w-1/2 mx-auto">
+    <Cart>lol</Cart>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import Cart from "@/components/Cart.vue";
+
+@Options({
+  components: { Cart },
+})
+export default class Host extends Vue {}
+</script>
